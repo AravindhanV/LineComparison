64a90488e87eb4f8038649fa1e570a5eb93bbaf7
Aravindhan V <aravindhan.v@perfios.com>
2021-08-20 16:23:15 +0530

    MODIFIED: Point is now a class

--- src/com/bridgelabz/linecomparison/LineComparison.java
+++ src/com/bridgelabz/linecomparison/LineComparison.java
@@ -1,10 +1,14 @@
 package com.bridgelabz.linecomparison;
 
+import com.bridgelabz.linecomparison.Point;
+
 public class LineComparison {
 	public static void main(String[] args) {
 		System.out.println("Welcome to Line Comparison Computation Program");
-		int x1=1,x2=3,y1=1,y2=3;
-		double len = Math.sqrt(Math.pow(x2-x1, 2)+Math.pow(y2-y1, 2));
+		Point p1 = new Point(1,1);
+		Point p2 = new Point(3,3);
+		
+		double len = Math.sqrt(Math.pow(p1.x-p2.x, 2)+Math.pow(p2.y-p1.y, 2));
 		System.out.print("Length of line: "+len);
 	}
 }
