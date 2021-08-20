460aac3c40d88d8edf2cba08349ce30f1938c75e
Aravindhan V <aravindhan.v@perfios.com>
2021-08-20 13:32:47 +0530

    ADDED: Length of line between two points

--- src/com/bridgelabz/linecomparison/LineComparison.java
+++ src/com/bridgelabz/linecomparison/LineComparison.java
@@ -3,5 +3,8 @@ package com.bridgelabz.linecomparison;
 public class LineComparison {
 	public static void main(String[] args) {
 		System.out.println("Welcome to Line Comparison Computation Program");
+		int x1=1,x2=3,y1=1,y2=3;
+		double len = Math.sqrt(Math.pow(x2-x1, 2)+Math.pow(y2-y1, 2));
+		System.out.print("Length of line: "+len);
 	}
 }
