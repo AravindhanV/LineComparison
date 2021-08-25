3980a8173696318a42c75c0363dff4cdb16657a7
Aravindhan V <aravindhan.v@perfios.com>
2021-08-25 18:40:18 +0530

    REFACTOR: Implemented OOP Concepts for Point and Line

--- src/com/bridgelabz/linecomparison/LineComparison.java
+++ src/com/bridgelabz/linecomparison/LineComparison.java
@@ -6,18 +6,22 @@ public class LineComparison {
 	public static void main(String[] args) {
 		System.out.println("Welcome to Line Comparison Computation Program");
 		//Line 1
-		Point p1 = new Point(1,1);
-		Point p2 = new Point(3,3);
-		Double len1 = p1.getDistance(p2);
+		Point point1 = new Point(1,1);
+		Point point2 = new Point(3,3);
+		Double length1 = point1.getDistance(point2);
 		
 		//Line 2
-		Point p3 = new Point(3,2);
-		Point p4 = new Point(4,4);
-		Double len2 = p3.getDistance(p4);
+		Point point3 = new Point(3,2);
+		Point point4 = new Point(4,4);
+		Double length2 = point3.getDistance(point4);
 		
-		if(len1.compareTo(len2)==0) {
+		compareLines(length1,length2);
+	}
+	
+	public static void compareLines(Double length1, Double length2) {
+		if(length1.compareTo(length2)==0) {
 			System.out.println("Lines are Equal");
-		} else if(len1.compareTo(len2)<0){
+		} else if(length1.compareTo(length2)<0){
 			System.out.println("Line 1 is smaller than Line 2");
 		} else {
 			System.out.println("Line 1 is longer than Line 2");
