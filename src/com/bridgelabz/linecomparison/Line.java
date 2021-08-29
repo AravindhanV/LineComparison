641a00da61e0d22201ee1d09fcc0d2af2f0bcb16
Aravindhan V <aravindhan.v@perfios.com>
2021-08-29 15:22:17 +0530

    ADDED: added Line class

--- src/com/bridgelabz/linecomparison/Line.java
+++ src/com/bridgelabz/linecomparison/Line.java
@@ -1,5 +1,15 @@
 package com.bridgelabz.linecomparison;
 
 public class Line {
-
+	private Point point1;
+	private Point point2;
+	
+	public Line(int x1,int y1,int x2,int y2) {
+		this.point1 = new Point(x1,y1);
+		this.point2 = new Point(x2,y2);
+	}
+	
+	public Double lineLength() {
+		return point1.getDistance(point2);
+	}
 }
--- src/com/bridgelabz/linecomparison/LineComparison.java
+++ src/com/bridgelabz/linecomparison/LineComparison.java
@@ -5,18 +5,11 @@ import com.bridgelabz.linecomparison.Point;
 public class LineComparison {
 	public static void main(String[] args) {
 		System.out.println("Welcome to Line Comparison Computation Program");
-		//Line 1
-		Point point1 = new Point(1,1);
-		Point point2 = new Point(3,3);
-		Double length1 = point1.getDistance(point2);
-		
-		//Line 2
-		Point point3 = new Point(3,2);
-		Point point4 = new Point(4,4);
-		Double length2 = point3.getDistance(point4);
+		Line line1 = new Line(1,1,3,3);
+		Line line2 = new Line(3,2,4,4);
 		
 		LengthComparisonIF lengthComparison = new LengthComparisonImpl();
-		lengthComparison.checkEqual(length1,length2);
-		lengthComparison.compareLength(length1, length2);
+		lengthComparison.checkEqual(line1.lineLength(),line2.lineLength());
+		lengthComparison.compareLength(line1.lineLength(), line2.lineLength());
 	}
 }
